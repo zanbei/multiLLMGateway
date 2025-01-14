cc82988b50b7c05c05cc14907877475256ceb1d8
DiscreteTom <discrete_tom@outlook.com>
2025-01-14 05:55:27 +0000

    feat(frontend/chat): calling bedrock is working

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -11,11 +11,77 @@ import {
 import { useState } from "react";
 import Messages from "./messages";
 import { ScrollableContainer } from "./common";
-import { INITIAL_MESSAGES } from "./config";
+import { INITIAL_MESSAGES, Message } from "./config";
+import {
+  BedrockRuntimeClient,
+  ConverseStreamCommand,
+} from "@aws-sdk/client-bedrock-runtime";
+
+const modelId = "anthropic.claude-3-haiku-20240307-v1:0";
 
 export default function Chat() {
   const [prompt, setPrompt] = useState("");
+  const [promptDisabled, setPromptDisabled] = useState(false);
   const [messages, setMessages] = useState(INITIAL_MESSAGES);
+
+  async function converse(messages: Message[]) {
+    const bedrock = new BedrockRuntimeClient({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: window.localStorage.getItem("AK")!,
+        secretAccessKey: window.localStorage.getItem("SK")!,
+      },
+    });
+
+    const response = await bedrock.send(
+      new ConverseStreamCommand({
+        modelId,
+        messages: messages
+          .slice(0, -1)
+          .filter((m) => m.type == "chat-bubble")
+          .map((message) => ({
+            role: message.authorId == "user" ? "user" : "assistant",
+            content: [{ text: message.content!.toString() }],
+          })),
+      })
+    );
+
+    for await (const event of response.stream!) {
+      const chunk = event.contentBlockDelta;
+      if (chunk?.delta?.text) {
+        const message = chunk.delta.text.toString();
+        if (message) {
+          messages.at(-1)!.content += message;
+          setMessages(messages);
+        }
+      }
+    }
+  }
+
+  function onPromptSend() {
+    if (prompt) {
+      const newMessages = [
+        ...messages,
+        { type: "chat-bubble", authorId: "user", content: prompt },
+        {
+          type: "chat-bubble",
+          authorId: "gen-ai",
+          content: "",
+          avatarLoading: true,
+        },
+      ] as Message[];
+      setMessages(newMessages);
+      setPrompt("");
+      setPromptDisabled(true);
+
+      converse(newMessages).finally(() => {
+        setPromptDisabled(false);
+        const msg = newMessages.at(-1)!;
+        if (msg.type == "chat-bubble") msg.avatarLoading = false;
+        setMessages(newMessages);
+      });
+    }
+  }
 
   return (
     <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
@@ -54,7 +120,8 @@ export default function Chat() {
                 <PromptInput
                   value={prompt}
                   onChange={({ detail }) => setPrompt(detail.value)}
-                  // onAction={onPromptSend}
+                  onAction={onPromptSend}
+                  disabled={promptDisabled}
                   actionButtonAriaLabel="Send"
                   actionButtonIconName="send"
                   placeholder="Ask a question"
