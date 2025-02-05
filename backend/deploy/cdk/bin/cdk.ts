fd31784c6aed662c5e5e6835f8cf6a4a9e9e1c08
zanbei <zhaoanbei@outlook.com>
2025-02-05 18:04:53 +0800

    cdk_together

--- backend/deploy/cdk/bin/cdk.ts
+++ backend/deploy/cdk/bin/cdk.ts
@@ -13,10 +13,16 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
 import { StackProps } from 'aws-cdk-lib'; 
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
-import * as path from 'path';
+// import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+// import * as path from 'path';
 import { RemovalPolicy } from 'aws-cdk-lib';
-import * as ecrdeploy from 'cdk-ecr-deployment';
+// import * as ecrdeploy from 'cdk-ecr-deployment';
+import * as servicediscovery from 'aws-cdk-lib/aws-servicediscovery';
+
+// 运行前export以下环境变量
+// export GLOBAL_AWS_SECRET_ACCESS_KEY='your_secret_access_key'
+// export GLOBAL_AWS_ACCESS_KEY_ID='your_access_key_id'
+// export DEEPSEEK_KEY='your_deepseek_key'
 
 const app = new cdk.App(); 
 
@@ -37,23 +43,46 @@ export class LitellmStack extends cdk.Stack {
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow HTTP traffic');
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(4000), 'Allow HTTPS traffic');
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306), 'Allow HTTPS traffic');
-    const suffix = Math.random().toString(36).substring(2, 6);
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8000), 'Allow HTTPS traffic');
 
-    const ecrRepo = new ecr.Repository(this, 'MyECR', {
-      repositoryName: `litellm-${suffix}`,
-      removalPolicy: RemovalPolicy.DESTROY, // 设置删除策略为 DESTROY
-      emptyOnDelete: true, // 可选：强制删除仓库时删除所有内容
+    // 创建公共 EC2 实例
+
+    // 使用 SSM 参数获取最新的 Ubuntu AMI
+    const machineImage = ec2.MachineImage.fromSsmParameter(
+      '/aws/service/canonical/ubuntu/server/focal/stable/current/amd64/hvm/ebs-gp2/ami-id',
+      {os: ec2.OperatingSystemType.LINUX,}
+    );
+
+    const instance = new ec2.Instance(this, 'bastion', {
+      instanceType: new ec2.InstanceType('t3.medium'), // 实例类型
+      machineImage: machineImage,
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }, // 使用公共子网
+      securityGroup,
+      keyName: 'nx2007' // 指定密钥对名称（不需要 .pem 后缀）
     });
 
-    const dockerImage = new ecr_assets.DockerImageAsset(this, 'litellm', {
-      directory: path.join(__dirname, '../../docker'),
-    });
+    // const suffix = Math.random().toString(36).substring(2, 6);
+    const suffix = 'ikvn'
 
-    // 将镜像推送到指定的 ECR 仓库
-    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
-      src: new ecrdeploy.DockerImageName(dockerImage.imageUri),
-      dest: new ecrdeploy.DockerImageName(`${ecrRepo.repositoryUri}:latest`), // 使用指定的仓库 URI 和标签
-    });
+    // china region 的 cdk-ecr-deployment 无法使用，原因是lambda无法下载public ecr
+
+    // const ecrRepo = new ecr.Repository(this, 'MyECR', {
+    //   repositoryName: `litellm-${suffix}`,
+    //   removalPolicy: RemovalPolicy.DESTROY, // 设置删除策略为 DESTROY
+    //   emptyOnDelete: true, // 可选：强制删除仓库时删除所有内容
+    // });
+
+    const ecrRepo = ecr.Repository.fromRepositoryName(this, 'MyECR', 'litellm');
+
+    // const dockerImage = new ecr_assets.DockerImageAsset(this, 'litellm', {
+    //   directory: path.join(__dirname, '../../docker'),
+    // });
+
+    // new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
+    //   src: new ecrdeploy.DockerImageName(dockerImage.imageUri),
+    //   dest: new ecrdeploy.DockerImageName(`${ecrRepo.repositoryUri}:latest`), // 使用指定的仓库 URI 和标签
+    // });
 
 
     const configBucket = new s3.Bucket(this, 'ConfigBucket', {
@@ -66,19 +95,6 @@ export class LitellmStack extends cdk.Stack {
       sources: [s3Deploy.Source.asset('../config')],
       destinationBucket: configBucket,
     });
-
-    // 创建 RDS Serverless 集群
-    // const rdsCluster = new rds.ServerlessCluster(this, `litellm_${suffix}`, {
-    //   engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_15_2 }),
-    //   vpc: vpc,
-    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
-    //   parameterGroup: rds.ParameterGroup.fromParameterGroupName(
-    //     this,
-    //     'ParameterGroup',
-    //     'default.aurora-postgresql15'),
-    //   scaling: { autoPause: cdk.Duration.minutes(10) },
-    //   defaultDatabaseName: 'litellm', 
-    // });
 
     // const rdsCluster = new rds.ServerlessCluster(this, `litellm-${suffix}`, {
     //   engine: rds.DatabaseClusterEngine.auroraPostgres({
@@ -96,6 +112,21 @@ export class LitellmStack extends cdk.Stack {
     const ecsTaskExecutionRole = new iam.Role(this, 'EcsTaskExecutionRole', {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
     });
+    // 创建 ECR 权限策略
+    const ecrPolicy = new iam.Policy(this, 'EcrPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            'ecr:GetAuthorizationToken',
+            'ecr:BatchCheckLayerAvailability',
+            'ecr:GetDownloadUrlForLayer',
+            'ecr:BatchGetImage',
+          ],
+          resources: ['*'], // 或者指定特定的 ECR ARN
+        }),
+      ],
+    });
+    ecrPolicy.attachToRole(ecsTaskExecutionRole);
 
     ecsTaskExecutionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'));
     ecsTaskExecutionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'));
@@ -122,27 +153,83 @@ export class LitellmStack extends cdk.Stack {
       }),
       environment: {
         GLOBAL_AWS_REGION: 'us-west-2',
-        GLOBAL_AWS_SECRET_ACCESS_KEY: '',
-        GLOBAL_AWS_ACCESS_KEY_ID: '',
+        GLOBAL_AWS_SECRET_ACCESS_KEY: process.env.GLOBAL_AWS_SECRET_ACCESS_KEY || '',
+        GLOBAL_AWS_ACCESS_KEY_ID: process.env.GLOBAL_AWS_ACCESS_KEY_ID || '',
         LITELLM_CONFIG_BUCKET_OBJECT_KEY: 'litellm_config.yaml',
         AWS_DEFAULT_REGION: 'cn-northwest-1',
         LITELLM_CONFIG_BUCKET_NAME: configBucket.bucketName,
         LITELLM_LOG: 'DEBUG',
-        DEEPSEEK_KEY: '',
+        DEEPSEEK_KEY: process.env.DEEPSEEK_KEY || '',
         DATA_BASE_URL: '',
       },
       portMappings: [{ containerPort: 4000 }],
     });
 
-    new ecs.FargateService(this, 'LitellmService', {
+    
+
+    // 创建 Cloud Map 命名空间
+    const namespace = new servicediscovery.PrivateDnsNamespace(this, 'MyNamespace', {
+      name: 'litellm.local',
+      vpc,
+    });
+
+    const LitellmService = new ecs.FargateService(this, 'LitellmService', {
       cluster: ecsCluster,
       taskDefinition,
       desiredCount: 1,
       assignPublicIp: true,
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC},
       securityGroups: [securityGroup], 
+      cloudMapOptions: {
+        cloudMapNamespace: namespace,
+        name: 'litellm', // 服务名称
+      },
     });
 
+    // 返回记录名称
+    const recordName = `${LitellmService.serviceName}.${namespace.namespaceName}`;
+
+    const ecrRepoProxy = ecr.Repository.fromRepositoryName(this, 'proxyrepo', 'proxy');
+
+    const taskDefinitionProxy = new ecs.FargateTaskDefinition(this, 'ProxyTask', {
+      taskRole: ecsTaskExecutionRole,
+      executionRole: ecsTaskExecutionRole,
+      cpu: 2048, // Increase to 2048 (2 vCPUs)
+      memoryLimitMiB: 4096, // Increase to 4096 (4 GB)
+    });
+
+    taskDefinitionProxy.addContainer('ProxyContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(ecrRepoProxy),
+      memoryLimitMiB: 2048,
+      cpu: 1024,
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: 'ecs',
+        logGroup: new logs.LogGroup(this, '/ecs/proxy'),
+      }),
+      environment: {
+        AWS_REGION_NAME: 'us-west-2',
+        OPENAI_API_URL: "http://${recordName}:4000/v1/chat/completions", // service discovery
+        AWS_SECRET_ACCESS_KEY: process.env.GLOBAL_AWS_SECRET_ACCESS_KEY || '',
+        AWS_ACCESS_KEY_ID: process.env.GLOBAL_AWS_ACCESS_KEY_ID || '',
+      },
+      portMappings: [{ containerPort: 4000 }],
+    });
+
+    const ProxyService = new ecs.FargateService(this, 'ProxyService', {
+      cluster: ecsCluster,
+      taskDefinition: taskDefinitionProxy,
+      desiredCount: 1,
+      assignPublicIp: true,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroups: [securityGroup], 
+      cloudMapOptions: {
+        cloudMapNamespace: namespace,
+        name: 'proxy', // 服务名称
+      },
+    });
+
+
+    const region = this.region;
     const regionMapping = new cdk.CfnMapping(this, 'regionMapping', {
       mapping: {
         'cn-north-1': {
