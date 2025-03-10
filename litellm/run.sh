c8a27a873a4497a65d076cdf91f419a897b76cfd
zanbei <145730733+zanbei@users.noreply.github.com>
2025-03-10 12:02:30 +0800

    Delete litellm directory

--- litellm/run.sh
+++
@@ -1,5 +0,0 @@
-docker run -d --rm --name litellm \
-    -v $(pwd)/litellm_config.yaml:/app/config.yaml \
-    -p 4000:4000 \
-    ghcr.io/berriai/litellm:main-v1.58.2 \
-    --config /app/config.yaml --detailed_debug
